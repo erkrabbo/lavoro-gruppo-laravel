cb9b0cc426fde003265d5f9f4bfbedc84bb24972
xVal4k <cristianbricicaru.cb@gmail.com>
2022-05-10 17:56:30 +0200

    trying to use style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('pageTitle', 'Home')
 
 @section('content')
-    <div class="container">
+    <div class="card">
         @foreach ($carList as $car)
             <h2>{{ $car->brand }}</h2>
             <p>{{ $car->model }}</p>
