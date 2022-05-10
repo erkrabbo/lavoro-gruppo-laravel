572846d13610834f8134c3093c6d4e8c77ce4486
aleballabio <96348218+aleballabio@users.noreply.github.com>
2022-05-10 17:13:32 +0200

    car protected  fillable

--- app/car.php
+++ app/car.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class car extends Model
 {
-
     public $timestamps = false;
+    protected $fillable = [
+        'model',
+        'brand',
+        'year'
+    ];
 }
