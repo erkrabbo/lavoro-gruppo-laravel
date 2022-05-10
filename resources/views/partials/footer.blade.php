a6afe821981f8fea0ca86f29853772fe07ebdde8
xVal4k <cristianbricicaru.cb@gmail.com>
2022-05-10 17:03:33 +0200

    blade commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <h2>
+        Sono il footer ma non sono nulla di speciale :(
+    </h2>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <h2>
+        Sono l' header ma sono poco speciale :(
+    </h2>
+</footer>
