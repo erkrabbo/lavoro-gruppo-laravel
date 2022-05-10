a93901658d7ed347669311eb6f8473c00ce862ed
xVal4k <cristianbricicaru.cb@gmail.com>
2022-05-10 17:57:20 +0200

    trying to use style 2.0

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,13 @@
 @section('pageTitle', 'Home')
 
 @section('content')
-    <div class="card">
+    <div class="container">
         @foreach ($carList as $car)
-            <h2>{{ $car->brand }}</h2>
-            <p>{{ $car->model }}</p>
-            <p>{{ $car->year }}</p>
+            <div class="card">
+                <h2>{{ $car->brand }}</h2>
+                <p>{{ $car->model }}</p>
+                <p>{{ $car->year }}</p>
+            </div>
         @endforeach
         {{ $carList->links() }}
     </div>
